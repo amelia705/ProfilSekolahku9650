793f564e6560a059f3e50fcc7500720fe73e419e
Dwiky16 <dwikydarmawan2004@gmail.com>
2022-02-24 13:11:52 +0700

    commit1

--- app/src/main/java/com/example/profilsekolahku9650/Buku8Activity.java
+++ app/src/main/java/com/example/profilsekolahku9650/Buku8Activity.java
@@ -1,4 +1,14 @@
 package com.example.profilsekolahku9650;
 
-public class Buku8Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Buku8Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buku8);
+    }
 }
